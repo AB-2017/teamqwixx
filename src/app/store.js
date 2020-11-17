54e1b16ae1453b9db550d78553d05221da63c177
Antoine Bertier <antoinebertier@MacBook-Air-2020-de-Antoine.local>
2020-11-17 15:42:37 +0100

    part of first release

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import playerReducer from '../features/player/playerSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    player: playerReducer,
   },
 });
